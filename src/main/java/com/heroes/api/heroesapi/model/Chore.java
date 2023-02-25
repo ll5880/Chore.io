6627010b0929c92fac69e41213e8c4228daf59ab
Patricio Solis <101016848+PatoSolis02@users.noreply.github.com>
2023-02-25 16:08:06 -0500

    Added classes for CreatorDoer, Chore, and Prize

---
+++ src/main/java/com/heroes/api/heroesapi/model/Chore.java
@@ -0,0 +1,47 @@
+package com.heroes.api.heroesapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents a Chore
+ * 
+ * @author Patricio Solis
+ */
+
+public class Chore {
+    @JsonProperty("choreName") private String choreName;
+    @JsonProperty("choreDesc") private String choreDesc;
+    @JsonProperty("points") private int points;
+
+    public Chore(@JsonProperty("choreName") String choreName, 
+        @JsonProperty("choreDesc") String choreDesc, @JsonProperty("points") int points){
+        this.choreName = choreName;
+        this.choreDesc = choreDesc;
+        this.points = points;
+    }
+
+    public String getChoreName(){
+        return this.choreName;
+    }
+
+    public String getChoreDesc(){
+        return this.choreDesc;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
+
+    public void setChoreName(String choreName){
+        this.choreName = choreName;
+    }
+
+    public void setChoreDesc(String choreDesc){
+        this.choreDesc = choreDesc;
+    }
+
+    public void setPoints(int points){
+        this.points = points;
+    }
+
+}
---
+++ src/main/java/com/heroes/api/heroesapi/model/CreatorDoer.java
@@ -0,0 +1,58 @@
+package com.heroes.api.heroesapi.model;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents a Creator and Doer of chores
+ * 
+ * @author Patricio Solis
+ */
+
+ public class CreatorDoer{
+    @JsonProperty("userName") private String userName;
+    @JsonProperty("points") private int points;
+    @JsonProperty("completedChores") private ArrayList<Chore> completedChores;
+    @JsonProperty("claimedPrizes") private ArrayList<Prize> claimedPrizes;
+
+    public CreatorDoer(@JsonProperty("userName") String userName){
+        this.userName = userName;
+        this.points = 0;
+        this.completedChores = new ArrayList<>();
+        this.claimedPrizes = new ArrayList<>();
+    }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
+
+    public ArrayList<Chore> getCompletedChores(){
+        return this.completedChores;
+    }
+
+    public ArrayList<Prize> getClaimedPrizes(){
+        return this.claimedPrizes;
+    }
+
+    public void completeChore(Chore chore){
+        this.completedChores.add(chore);
+        this.points += chore.getPoints();
+    }
+
+    public void claimPrize(Prize prize){
+        this.claimedPrizes.add(prize);
+        this.points -= prize.getPointCost();
+    }
+
+    public Prize redeemPrize(){
+        if(claimedPrizes.size() > 0){
+            return this.claimedPrizes.remove(0);
+        }
+        return null;
+    }
+ }
